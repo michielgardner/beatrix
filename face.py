f4d0cf41dec1095750d5c1f093acfe583f48f7a6
=Tom Aizenberg <=tom.aizenberg@gmail.com>
2012-04-25 14:37:55 +0200

    Face detection and pasting stuff works now

---
+++ face.py
@@ -0,0 +1,68 @@
+import sys, os
+import opencv
+from opencv.cv import *
+from opencv.highgui import *
+from PIL import Image
+
+def detectObjects(image):
+  """Converts an image to grayscale and prints the locations of any 
+     faces found"""
+  grayscale = cvCreateImage(cvSize(image.width, image.height), 8, 1)
+  cvCvtColor(image, grayscale, CV_BGR2GRAY)
+  
+  storage = cvCreateMemStorage(0)
+  cvClearMemStorage(storage)
+  cvEqualizeHist(grayscale, grayscale)
+
+  cascade = cvLoadHaarClassifierCascade(
+    'haarcascade_frontalface_default.xml',
+    cvSize(1,1))
+
+  faces = cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2,
+                             CV_HAAR_DO_CANNY_PRUNING, cvSize(50,50))
+
+  positions = []
+
+  if faces:
+    positions = [(f.x, f.y, f.width, f.height) for f in faces]
+  
+  return positions
+
+def find_faces_and_replace(in_file, replace_file, out_file):
+  
+  # Load image
+  main_image = cvLoadImage(in_file);
+
+  # Get the positions of the faces
+  positions = detectObjects(main_image)  
+  
+  # Open main image
+  image = Image.open(in_file)
+
+  # Get the replace face
+  face_replace = Image.open(replace_file)
+
+  # make sure directory exists
+  ensure_dir(out_file.rsplit("/", 1))
+
+  # Cover every face with the TROLL
+  for face in positions:
+        
+    # Get size and position
+    position_xy = (face[0],face[1])
+    size = (face[2],face[3])
+
+    # Resize overlay
+    f = face_replace.resize(size)
+
+    # Paste
+    image.paste(f, position_xy, f)
+
+  image.save(out_file)
+
+if __name__ == "__main__":
+  in_file = sys.argv[1]
+  replace_file = sys.argv[2]
+  out_file = sys.argv[3]
+  find_faces_and_replace(in_file, replace_file, out_file)
+
