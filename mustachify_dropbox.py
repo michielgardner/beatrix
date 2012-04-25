d4f98d2e40749fa6155ece757d65e905bd32b87b
Michiel Gardner <michiel@gardner.nu>
2012-04-25 15:57:15 +0200

    yaaay

--- mustachify_dropbox.py
+++ mustachify_dropbox.py
@@ -2,6 +2,7 @@
 from dropbox import client, rest, session
 from bottle import route, run, request
 import requests
+import shelve
 
 APP_KEY = 'vprl6knhaaceson'
 APP_SECRET = '4d0iffqqjktu1i1'
@@ -9,23 +10,27 @@ ACCESS_TYPE = 'dropbox' # ACCESS_TYPE should be 'dropbox' or 'app_folder' as con
 
 @route('/')
 def show_authorize_url():
-  sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
-  request_token = sess.obtain_request_token()
-
-  return sess.build_authorize_url(request_token, oauth_callback='http://localhost:8080/mustachify/')
-
-@route('/mustachify/')
-def mustachify():
-  uid = request.GET.get('uid')
   oauth_token = request.GET.get('oauth_token')
-  
-  access_token = sess.obtain_access_token(request_token)
-  #client = client.DropboxClient(sess)
 
-  #for search_result in client.search('TNW2012', '.jpg'):
-  #  dropbox_path = search_result['path']
-  #  file_obj = requests.get('http://mustachify.me/?src=%s' % client.media(search_result['path'])['url']).content
+  sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
+  d = shelve.open('tokens')
 
-  #  client.put_file(dropbox_path, file_obj, overwrite=True)
+  if oauth_token is None:
+    request_token = sess.obtain_request_token()
+    d[request_token.key] = request_token
+  else:
+    #sess.set_token(access_token_key, d[access_token_key])
+    token = sess.obtain_access_token(d[oauth_token])
+    c = client.DropboxClient(sess)
+
+    for search_result in c.search('TNW2012', '.jpg'):
+      dropbox_path = search_result['path']
+      file_obj = requests.get('http://mustachify.me/?src=%s' % c.media(search_result['path'])['url']).content
+      c.put_file(dropbox_path, file_obj) #, overwrite=True)
+
+  d.close()
+
+  if oauth_token is None:
+    return '<a href="%s">Link</a>' % sess.build_authorize_url(request_token, oauth_callback='http://localhost:8080/')
 
 run()
